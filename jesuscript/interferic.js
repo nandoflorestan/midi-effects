72fd1b4bd954d9821f318de41fd65bcad6328f8d
Nando Florestan <nandoflorestan@gmail.com>
2021-12-31 11:50:26 +0100

    Update interferic.js

--- jesuscript/interferic.js
+++ jesuscript/interferic.js
@@ -1,5 +1,10 @@
 desc: CC to interfere with dynamic CC
-// written by Nando Florestan 
+// written by Nando Florestan.
+// This effects eats up CC4 messages.  It uses these to make relative changes to CC1 values.
+// The CC1 output is given by CC1value + CC4value - 64.
+// You can keep CC4 centered at 64 and occasionally change it to correct CC1 while
+// keeping these corrections separate from the main CC1 signal.
+// TODO: Make this affect a range of channels, not just one.
 
 // MIDI-only effect
 in_pin:none
@@ -8,7 +13,7 @@ out_pin:none
 slider1:  1 <1,  16, 1> Channel
 slider2: 11 <1, 127, 1> Dynamic CC
 slider3: 64 <0, 127, 1> Dynamic value
-slider4:  5 <1, 127, 1> Interferic CC
+slider4:  4 <1, 127, 1> Interferic CC
 slider5: 64 <0, 127, 1> Interferic value
 slider6: 64 <0, 127, 1> Result
 
@@ -40,7 +45,7 @@ while (midirecv (blockOffset, status, databytes) ? (
 		CCnum == dynamic_cc  ||  CCnum == interferic_cc ? (
 			// Send the modified CC value
 			// result = dynamic_value + interferic_value - 64;
-			result = min(127, max(0, (dynamic_value + (interferic_value / 2 - 32)))) | 0;
+			result = min(127, max(0, (dynamic_value + (interferic_value - 64)))) | 0;
 			slider6 = result;
 			midisend(blockOffset, status | 0, (result * 256 + dynamic_cc) | 0);
 		) : (
